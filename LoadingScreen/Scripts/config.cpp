c3271e408c4b20b2b2153d6d983367161884c252
jonni111 <101236664+jonni111@users.noreply.github.com>
2023-08-22 22:39:37 +0500

    Add files via upload

---
+++ LoadingScreen/Scripts/config.cpp
@@ -0,0 +1,48 @@
+class CfgPatches
+{
+	class DayZExpansion_LoadingScreen_Scripts
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"DZ_Scripts"
+		};
+	};
+};
+class CfgMods
+{
+	class DZ_Expansion_LoadingScreen
+	{
+		dir="LoadingScreen";
+		credits="Steve aka Salutesh";
+		extra=0;
+		type="mod";
+		dependencies[]=
+		{
+			"Game",
+			"World",
+			"Mission"
+		};
+		class defs
+		{
+			class gameScriptModule
+			{
+				value="";
+				files[]=
+				{
+					"LoadingScreen/Scripts/3_Game"
+				};
+			};
+			class missionScriptModule
+			{
+				value="";
+				files[]=
+				{
+					"MyMod/Scripts/5_Mission"
+				};
+			};
+		};
+	};
+};
